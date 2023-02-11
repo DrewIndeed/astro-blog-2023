4bfab32a7df601a12c3222418b65adec4b81a809
an.le6 <anle.drewindeed@gmail.com>
2023-02-11 20:13:03 +0700

    23. integrate Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,9 +13,12 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-cip.netlify.app",
   integrations: [image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  }), sitemap(), react(), tailwind()]
+  }), sitemap(), react(), tailwind(), partytown()]
 });
